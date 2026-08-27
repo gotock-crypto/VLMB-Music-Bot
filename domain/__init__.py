41ea89224b81f9aff1c00b6272b8641cd5ce8542
gotock-crypto <gotock@gmail.com>
2026-08-27 14:54:42 +0300

    refactor: expose TrackInfo from domain boundary

--- domain/__init__.py
+++ domain/__init__.py
@@ -1,3 +1,4 @@
 from .models import Track, DownloadResult
+from .track_info import TrackInfo, track_uid_from_any
 from .errors import DomainError, InvalidTransition, UnknownCallback, ProviderDomainError
-__all__ = ["Track", "DownloadResult", "DomainError", "InvalidTransition", "UnknownCallback", "ProviderDomainError"]
+__all__ = ["Track", "DownloadResult", "TrackInfo", "track_uid_from_any", "DomainError", "InvalidTransition", "UnknownCallback", "ProviderDomainError"]
